e0dbb48e63c8dd6fabff4adafc98dd183b17b104
Gareth Lloyd <glloyd@glloyd-laptop.(none)>
2011-01-22 15:55:23 +0000

    sketch out functionality of page_parser.py

--- wikipedia_processor/page_parser.py
+++ wikipedia_processor/page_parser.py
@@ -6,12 +6,23 @@ from xml.sax.handler import feature_namespaces
 from xml.sax.saxutils import escape, XMLFilterBase
 from unicodedata import normalize
 
+class Coords(object):
+    def __init__(self, lat=None, long=None):
+        self.lat = lat
+        self.long = long
+
 class WikipediaPage(object):
     def __init__(self):
         self.title = u''
         self.id = u''
         self.text = u''
-        self.coords = u''
+        self.coords = []
+
+    def containsCoords(self):
+        return False
+
+    def isYear(self):
+        return False
 
     def __str__(self):
         return 'ID %s TITLE %s' % (self.id.encode('utf_8'), self.title.encode('utf_8'))
@@ -50,45 +61,56 @@ def _wrap_complete(method_name):
 for n in '''startElement endElement endDocument'''.split():
     _wrap_complete(n)
 
+def processYear(text):
+    pass
+
+def processPage(text):
+    pass
 
 class WikipediaHandler(handler.ContentHandler):
-        def __init__(self, out=sys.stdout):
-            handler.ContentHandler.__init__(self)
-            self._out = out
-            self.currentPage = None
-            self.currentTag = ''
+    def __init__(self, out=sys.stdout):
+        handler.ContentHandler.__init__(self)
+        self._out = out
+        self.currentPage = None
+        self.currentTag = ''
+        self.ignoreIdTags = False
+
+    def startElement(self, name, attrs):
+        self.currentTag = name
+        if (name == 'page'):
+            # add a page
+            self.currentPage = WikipediaPage()
+        elif (name == 'revision'):
+            # when we're in revision, ignore ids
+            self.ignoreIdTags = True
+
+    def endElement(self, name):
+        if (name == 'page'):
+            self.analysePage()
+        elif (name == 'revision'):
+            # we've finished the revision section
             self.ignoreIdTags = False
+        self.currentTag = ''
 
-        def startElement(self, name, attrs):
-            self.currentTag = name
-            if (name == 'page'):
-                # add a page
-                self.currentPage = WikipediaPage()
-            elif (name == 'revision'):
-                # when we're in revision, ignore ids
-                self.ignoreIdTags = True
+    def characters(self, content):
+        if (self.currentTag == 'id' and not self.ignoreIdTags):
+            self.currentPage.id = content
+        elif (self.currentTag == 'title'):
+            self.currentPage.title = content
+        elif self.currentTag == 'text':
+            self.currentPage.text += content
 
-        def endElement(self, name):
-            if (name == 'page'):
-                print self.currentPage
-            elif (name == 'revision'):
-                # we've finished the revision section
-                self.ignoreIdTags = False
-            self.currentTag = ''
-
-        def characters(self, content):
-            if (self.currentTag == 'id' and not self.ignoreIdTags):
-                self.currentPage.id = content
-            elif (self.currentTag == 'title'):
-                self.currentPage.title = content
-            elif self.currentTag == 'text':
-                self.currentPage.text += content
+    def analysePage(self):
+        if (containsCoords(self.currentPage.text)):
+            processPage(self.currentPage)
+        if (isYear(self.currentPage.text)):
+            processYear(self.currentPage)
 
 if __name__ == '__main__':
     parser = make_parser()
     # Tell the parser we are not interested in XML namespaces
     parser.setFeature(feature_namespaces, 0)
-    
+
     wh = WikipediaHandler()
 
     # apply the text_normalize_filter

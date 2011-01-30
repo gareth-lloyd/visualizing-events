372f1f242f31f7379e3a5a60b54ae2207593a831
Gareth Lloyd <glloyd@glloyd-laptop.(none)>
2011-01-30 23:26:35 +0000

    hacked around the output script

--- wikipedia_processor/output_csv.py
+++ wikipedia_processor/output_csv.py
@@ -1,36 +1,40 @@
+from pymongo import ASCENDING, DESCENDING
 from events_processor import DataSource
 d = DataSource()
 
-JSON_FMT = '{"year" : %s, "title" : "%s", "latitude": %s, "longitude": %s, "month": %d, "day": %d, "eventText": %s}'
+JSON_FMT = '{"title" : "%s", "latitude": %s, "longitude": %s, "year" : %s, "month": %s, "day": %s, "eventText": "%s"}'
 CSV_FMT = '"%s","%s %s","%s","%s","%s","%s"'
 fmt = JSON_FMT
 
+# apply a bounding box to lats and longs
 MIN_LAT = -90
 MAX_LAT = 90
 MIN_LONG = -180
 MAX_LONG = 180
 
 # drop and recreate the reference counts
-d.db.drop_collection("ref_counts")
+#d.db.drop_collection("ref_counts")
 refCountCollection = d.db.ref_counts
 
-# count references to pages
-for event in eventCollection.find().sort([("year", pymongo.ASCENDING), ("month", pymongo.ASCENDING), ("day", pymongo.ASCENDING)]):
-    for link in event['links']:
-        p = pageCollection.find_one({"_id" : link})
-        if p:
-            ref = refCountCollection.find_one({"_id" : p["_id"]})
-            if not ref:
-                refCountCollection.insert({"_id" : p["_id"], "refs": 1})
-            else:
-                refCountCollection.update({'_id': p["_id"]}, {"$inc" : { "refs": 1 }})
+## count references to pages
+#for event in d.eventCollection.find():
+#    for link in event['links']:
+#        p = d.pageCollection.find_one({"_id" : link})
+#        if p:
+#            ref = refCountCollection.find_one({"_id" : p["_id"]})
+#            if not ref:
+#                refCountCollection.insert({"_id" : p["_id"], "refs": 1})
+#            else:
+#                refCountCollection.update({'_id': p["_id"]}, {"$inc" : { "refs": 1 }})
+
+print refCountCollection.count()
 
 # output location-linked events. Prefer less-linked pages
-for event in eventCollection.find().sort([("year", pymongo.ASCENDING), ("month", pymongo.ASCENDING), ("day", pymongo.ASCENDING)]):
+for event in d.eventCollection.find().sort([("year", ASCENDING), ("month", ASCENDING), ("day", ASCENDING)]):
     pageToLink = None
     leastRefs = 1000000
     for link in event['links']:
-        p = pageCollection.find_one({"_id" : link})
+        p = d.pageCollection.find_one({"_id" : link})
         if p:
             ref = refCountCollection.find_one({"_id" : p["_id"]})
             if ref["refs"] < leastRefs:
@@ -39,4 +43,8 @@ for event in eventCollection.find().sort([("year", pymongo.ASCENDING), ("month",
         eventText = event["event_text"].replace(',', '').encode('utf-8')
         title = pageToLink["_id"].replace(',', '').encode('utf-8')
         coord = pageToLink["coords"][0]
-        print fmt % (title, coord["latitude"], coord["longitude"], event["year"], event["month"], event["day"], eventText)
+        latitude, longitude = coord["latitude"], coord["longitude"]
+        month = event["month"] if event["month"] else 0
+        day = event["day"] if event["day"] else 0
+        if (MIN_LAT <= latitude <= MAX_LAT) and (MIN_LONG <= longitude <= MAX_LONG):
+            print fmt % (title, latitude, longitude, event["year"], month, day, eventText)
